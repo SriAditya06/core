d58c502b6822ea46c220c5a55213c6c3a98f3fde
SriAditya06 <145857932+SriAditya06@users.noreply.github.com>
2023-10-07 20:16:57 +0200

    Update __init__.py
    Refactored this function to reduce its Cognitive Complexity from 31 to 15

--- homeassistant/components/netatmo/__init__.py
+++ homeassistant/components/netatmo/__init__.py
@@ -63,8 +63,6 @@ from .const import (
     WEBHOOK_DEACTIVATION,
     WEBHOOK_PUSH_TYPE,
 )
-from .data_handler import NetatmoDataHandler
-from .webhook import async_handle_webhook
 
 _LOGGER = logging.getLogger(__name__)
 
